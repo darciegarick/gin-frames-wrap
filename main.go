78bbf501b6caaaa3a4166cefd156c108407b409a
zhuziying <1249353194@qq.com>
2024-03-15 16:54:17 +0800

    静态资源处理&优雅重启服务器

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/showmebug/my-gin-demo/bootstrap"
 	"github.com/showmebug/my-gin-demo/global"
 )
@@ -24,13 +23,6 @@ func main() {
 		}
 	}()
 
-	router := gin.Default()
-	// 定义路由
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World!",
-		})
-	})
-	// 启动服务
-	router.Run(":" + global.App.Config.App.Port)
+	// 启动服务器
+	bootstrap.RunServer()
 }
