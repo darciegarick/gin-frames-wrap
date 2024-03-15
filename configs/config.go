3c680a385535e53fe114ea99912ff4aebdc672c4
zhuziying <1249353194@qq.com>
2024-03-15 16:04:00 +0800

    数据库初始化

--- configs/config.go
+++ configs/config.go
@@ -1,8 +1,9 @@
 package configs
 
 type Configuration struct {
-	App App `mapstructure:"app" json:"app" yaml:"app"`
-	Log Log `mapstructure:"log" json:"log" yaml:"log"`
+	App      App      `mapstructure:"app" json:"app" yaml:"app"`
+	Log      Log      `mapstructure:"log" json:"log" yaml:"log"`
+	Database Database `mapstructure:"database" json:"database" yaml:"database"`
 }
 
 type App struct {
--- global/app.go
+++ global/app.go
@@ -4,12 +4,14 @@ import (
 	"github.com/showmebug/my-gin-demo/configs"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 )
 
 type Application struct {
 	ConfigViper *viper.Viper
 	Config      configs.Configuration
 	Log         *zap.Logger
+	DB          *gorm.DB
 }
 
 var App = new(Application)
